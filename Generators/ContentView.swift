64cb4fdf5612f565b0d212d194a79280e711489f
Yaroslav <kukhar.yr@gmail.com>
2020-01-11 19:29:24 +0200

    Initial Commit

---
+++ Generators/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Generators
+//
+//  Created by Slavik on 11.01.2020.
+//  Copyright © 2020 Slavik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
