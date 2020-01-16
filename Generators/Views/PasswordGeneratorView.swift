48422c977c7ce6d113a8ec94f24b8f262b13e0bb
Yaroslav <kukhar.yr@gmail.com>
2020-01-16 20:47:53 +0200

    change structure of project

---
+++ Generators/Views/PasswordGeneratorView.swift
@@ -0,0 +1,21 @@
+//
+//  PasswordGeneratorView.swift
+//  Generators
+//
+//  Created by Slavik on 16.01.2020.
+//  Copyright © 2020 Slavik. All rights reserved.
+//
+
+import SwiftUI
+
+struct PasswordGeneratorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PasswordGeneratorView_Previews: PreviewProvider {
+    static var previews: some View {
+        PasswordGeneratorView()
+    }
+}
